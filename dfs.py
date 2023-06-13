0063cd4fa694100ededf54bce5b75264006fc85d
Danisimoso <danila_sidorenko@mail.ru>
2023-06-13 18:07:50 +0300

    Add new once сomments

--- dfs.py
+++ dfs.py
@@ -1,8 +1,8 @@
 def dfs(graph, start, len, visited=None):
     try:
         if visited is None:  #проверка посещена ли вершина
-            visited = set()  
-        visited.add(start)   
+            visited = set() #посетил1
+        visited.add(start)   #создал2
     except Exception as e:
         print ("Node is not valid" + start)
         raise e
